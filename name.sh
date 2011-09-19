ca12971f93b79982b8e8c7ebfb1cea365904bb15
Rafi Witten <rafiwitten@gmail.com>
2011-09-19 15:26:37 -0400

    Added name.sh

---
+++ name.sh
@@ -0,0 +1,23 @@
+# invocation is ./name.sh ${algorithm} ${class} ${C} ${foldnum} ${randomness} ${hallu} ${kernel} ${prox_weight}
+
+algorithm=$1
+class=$2
+C=$3
+foldnum=$4
+randomness=$5
+hallu=$6
+kernel=$7
+prox_weight=$8
+
+algorithms[1]='cccp'
+algorithms[2]='spl'
+algorithms[3]='splplus'
+
+hallus[0]='nohallu'
+hallus[1]='hallu'
+
+kernels[0]='bow'
+kernels[1]='spm'
+
+echo ${algorithms[$algorithm]}_${class}_C${C}_fold${foldnum}_rand${randomness}_${hallus[$hallu]}_${kernels[$kernel]}_lambda${prox_weight}
+
